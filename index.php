c2acc56c3319e63032c230fb93283a4440efc775
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-05-12 18:21:18 -0300

    Começo da atividade, criação dos arquivos, feito: index.php e conexao.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+include('conexao.php');
+
+$sql = "SELECT * FROM alunos";
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Aluno</title>
+</head>
+<body>
+    <h1>Cadastrar Aluno</h1>
+    <form action="inserir.php" method="POST">
+        Nome: <input type="text" name="nome" required><br>
+        Email: <input type="email" name="email" required><br>
+        <button type="submit">Cadastrar</button>
+    </form>
+
+    <h2>Alunos Cadastradis</h2>
+    <ul>
+        <?php while ($aluno = $result->fetch_assoc()): ?>
+            <li>
+                <?php echo $aluno['nome'] . " - " . $aluno['email']; ?>
+                <a href="excluir.php?id=<?php echo $aluno['id']; ?>" onclick="return confirm ('Tem certeza que deseja excluir?')">[Excluir]</a>
+            </li>
+            <?php endwhile; ?>
+    </ul>
+</body>
+</html>
