64df33e6c20196e07583eddc172b08de4cde0a91
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-05-12 18:32:26 -0300

    Terminado a parte de deletar informações inseridas no banco de dados.

---
+++ excluir.php
@@ -0,0 +1,15 @@
+<?php
+include('conexao.php');
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM alunos WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    header("Location: index.php");
+    exit();
+} else {
+    echo "Erro ao excluir: " . $conn->error;
+}
